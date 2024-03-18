c2d9ab406575b50619a50cf8b4ad7d6e68604a21
requiem <macbookpro@MacBook-Pro-MacBook.local>
2024-03-18 12:37:07 +0200

    pr2.1

--- pr2.1.cpp
+++ pr2.1.cpp
@@ -1,3 +1,31 @@
-//
-// Created by MacBook Pro on 18.03.2024.
-//
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printArray(int arr[], int  size) {
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int X[] = {-3,4,1,7,8,-12,9,-5,34,-10};
+    int Y[10];
+    int M = 5;
+    int yIndex = 0;
+
+    cout << "array x: ";
+    printArray(X,10);
+    cout << "M = " << M << endl;
+
+    for (int i = 0; i < 10; ++i) {
+        if (abs(X[i]) > M) {
+            Y[yIndex++] = X[i];
+        }
+    }
+    cout << "array Y: ";
+    printArray(Y,yIndex);
+
+    return 0;
+}
