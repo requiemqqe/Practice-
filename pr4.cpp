6f8a48ee1a7e589ef616f7c6db1e50b58f08cb33
requiem <macbookpro@MacBook-Pro-MacBook.local>
2024-03-21 10:28:27 +0200

    pr4

--- pr4.cpp
+++ pr4.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 using namespace std;
 
+void reversedArray(int arr[], int n) {
+    for (int i = 0; i < n/2; i++) {
+        int temp = arr[i];
+        arr[i] =  arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+}
+
+bool isSorted(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < arr[n - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int binarySearch(int arr[], int left, int right, int target) {
     while (left <= right) {
         int mid = left + (right - left) / 2;
+        cout << "Left: " << left << ", Right: " << right << ", Mid: " << mid << endl;
 
         if (arr[mid] == target)
             return mid;
@@ -26,6 +44,18 @@ int main() {
     cout << "Enter num for search:" << endl;
     int target;
     cin >> target;
+
+    int x;
+    cout << "Enter x: ";
+    cin >> x;
+
+    reversedArray(arr,10);
+    cout << "Array reversed";
+    if (isSorted(arr,10)) {
+        cout << "Array is sorted." << endl;
+    } else {
+        cout << "Array is not sorted." << endl;
+    }
 
     int index = binarySearch(arr, 0, 9, target);
 
